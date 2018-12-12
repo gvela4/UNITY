ecd10cfaad39b2ae33a44f73314be2bf81d1e24d
Gerardo Vela <gvelaxv@gmail.com>
2018-12-11 23:30:38 -0700

    added scripts, game done

---
+++ Golem and Pumpkins/Assets/Scenes/Scripts/GolemMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemMovement : MonoBehaviour {
+
+    private Rigidbody myBody;
+    private float moveForce = 10f; // to move the golem
+
+	// Use this for initialization // similar to Start
+	void Awake ()
+    {
+        myBody = GetComponent<Rigidbody>(); // gets the component attached to the rigibody
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal"); // gets arrows keys or A/D keys
+        myBody.velocity = new Vector3(-h * moveForce, 0f, 0f); // only moves on X axis
+	}
+}
---
+++ Golem and Pumpkins/Assets/Scenes/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+    public GameObject pumpkin;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(StartSpawing());
+    }
+    // called in timely manner
+    IEnumerator StartSpawing()
+    {
+        yield return new WaitForSeconds (Random.Range(1f, 3.5f)); // a random time between 1 and 3.5 seconds
+        Instantiate (pumpkin, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity); // used to create a duplicate out of the game object
+
+        // creates a pumpkin object at a position(Range) using a position(Quaternion).
+
+        StartCoroutine(StartSpawing());
+    }
+
+}
