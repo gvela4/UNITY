2a46e1ddf825d1bc149d25643ba94486ea7fc4d5
Gerardo Vela <gvelaxv@gmail.com>
2019-01-22 23:28:47 -0700

    Enemy health

---
+++ Awesome Knight/Awesome Knight/Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        print("Enemy Took damage, health is"+ health);
+        Debug.Log("Enemy Took damage, health is" + health);
+        
+
+        if (health <= 0)
+        {
+
+        }
+    }
+}
